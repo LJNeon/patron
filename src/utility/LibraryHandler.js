64c65b384a5c430b5641977b6ae4e72980709659
vim2meta <vim2meta@tutanota.com>
2017-10-23 10:49:23 -0400

    might aswel be else if because of the return inside the if

--- src/utility/LibraryHandler.js
+++ src/utility/LibraryHandler.js
@@ -10,18 +10,14 @@ class LibraryHandler {
       case 'discord.js':
         if (command.memberPermissions.length > 0 && message.member.hasPermission(command.memberPermissions) === false) {
           return Constants.results.memberPermissions(command, command.memberPermissions);
-        }
-
-        if (command.botPermissions.length > 0 && message.guild.me.hasPermission(command.botPermissions) === false) {
+        } else if (command.botPermissions.length > 0 && message.guild.me.hasPermission(command.botPermissions) === false) {
           return Constants.results.botPermissions(message.client, command, command.botPermissions);
         }
         break;
       case 'eris':
         if (command.memberPermissions.length > 0 && command.memberPermissions.every((permission) => message.member.permission.has(permission) === true) === false) {
           return Constants.results.memberPermissions(command, command.memberPermissions);
-        }
-
-        if (command.botPermissions.length > 0 && command.botPermissions.every((permission) => message.channel.guild.members.get(message._client.user.id).permission.has(permission) === true) === false) {
+        } else if (command.botPermissions.length > 0 && command.botPermissions.every((permission) => message.channel.guild.members.get(message._client.user.id).permission.has(permission) === true) === false) {
           return Constants.results.botPermissions(message.client, command, command.botPermissions);
         }
         break;
