598bfe7e987f7f3ded2b2b9b9eac38bf92cdb23e
El Jay <ljninjaa@gmail.com>
2017-10-24 02:55:38 +0000

    John's allergic to undefined, changed it to null

--- src/extensions/eris/Message.js
+++ src/extensions/eris/Message.js
@@ -2,6 +2,6 @@ const eris = require('eris');
 
 Object.defineProperty(eris.Message.prototype, 'guild', {
   get: function () {
-    return this.channel.guild;
+    return this.channel.guild !== undefined ? this.channel.guild : null;
   }
 });
