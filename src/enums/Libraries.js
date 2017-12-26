054a5b7c3be8215bc7817764d06d1fd286cff383
vim2meta <vim2meta@tutanota.com>
2017-12-26 14:30:31 -0500

    correct naming (#8)
    * Various additions
    
    * Fixed typo
    
    * Fixed index.js
    
    * Correct naming
    
    Makes zero sense to make an enum of options, should just be an enum of libraries, and prevents redundancy in enum members.

---
+++ src/enums/Libraries.js
@@ -0,0 +1,14 @@
+/**
+ * The supported libraries in patron.
+ * @enum {Symbol}
+ * @readonly
+ */
+
+const Libraries = {
+  /** The eris library */
+  Eris: Symbol(),
+  /** The discord.js library */
+  DiscordJS: Symbol()
+};
+
+module.exports = Libraries;
