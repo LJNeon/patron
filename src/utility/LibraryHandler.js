f41a9e6ad680233f4e50b9d93d314dfa0d221e15
El Jay <ljninjaa@gmail.com>
2018-10-08 20:42:05 +0000

    fix perm checks in DMs

--- src/utility/LibraryHandler.js
+++ src/utility/LibraryHandler.js
@@ -61,14 +61,18 @@ class LibraryHandler {
   validatePermissions(command, message) {
     switch (this.library) {
       case 'discord.js':
-        if (command.memberPermissions.length > 0 && message.member.hasPermission(command.memberPermissions) === false) {
+        if (message.channel.type !== 'text') {
+          return;
+        } else if (command.memberPermissions.length > 0 && message.member.hasPermission(command.memberPermissions) === false) {
           return Constants.results.memberPermissions(command, command.memberPermissions);
         } else if (command.botPermissions.length > 0 && message.guild.me.hasPermission(command.botPermissions) === false) {
           return Constants.results.botPermissions(message.client, command, command.botPermissions);
         }
         break;
       case 'eris':
-        if (command.memberPermissions.length > 0 && command.memberPermissions.every((permission) => message.member.permission.has(permission) === true) === false) {
+        if (message.channel.type !== DiscordChannelType.TextChannel) {
+          return;
+        } else if (command.memberPermissions.length > 0 && command.memberPermissions.every((permission) => message.member.permission.has(permission) === true) === false) {
           return Constants.results.memberPermissions(command, command.memberPermissions);
         } else if (command.botPermissions.length > 0 && command.botPermissions.every((permission) => message.channel.guild.members.get(message._client.user.id).permission.has(permission) === true) === false) {
           return Constants.results.botPermissions(message._client, command, command.botPermissions);
