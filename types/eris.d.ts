ad0fd57ff124f7b66e8552322d6919a3e9d8e78e
LJNeon <ljninjaa@gmail.com>
2020-02-06 11:35:49 -0700

    Potential infinite optional arg fix + typings improvement

--- types/eris.d.ts
+++ types/eris.d.ts
@@ -1,11 +1,5 @@
+import {Constants} from "eris";
+
 export {Message} from "eris";
 
-export type PermissionName = "createInstantInvite" | "kickMembers" | "banMembers" | "administrator"
-  | "manageChannels" | "manageGuild" | "addReactions" | "viewAuditLogs"
-  | "voicePrioritySpeaker" | "stream" | "readMessages"
-  | "sendMessages" | "sendTTSMessages" | "manageMessages" | "embedLinks"
-  | "attachFiles" | "readMessageHistory" | "mentionEveryone" | "externalEmojis"
-  | "viewGuildAnalytics" | "voiceConnect" | "voiceSpeak" | "voiceMuteMembers"
-  | "voiceDeafenMembers" | "voiceMoveMembers" | "voiceUseVAD" | "changeNickname"
-  | "manageNicknames" | "manageRoles" | "manageWebhooks" | "manageEmojis"
-  | "all" | "allGuild" | "allText" | "allVoice";
+export type PermissionName = keyof Constants["Permissions"];
